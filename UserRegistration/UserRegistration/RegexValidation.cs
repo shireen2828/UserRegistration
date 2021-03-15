cb411e4f780823bb58c0f4abaae142cb0fcaecd5
unknown <shireenkk28@gmail.com>
2021-03-16 00:55:04 +0530

    [shireen]Added Password Rule1

--- UserRegistration/UserRegistration/RegexValidation.cs
+++ UserRegistration/UserRegistration/RegexValidation.cs
@@ -11,6 +11,7 @@ namespace UserRegistration
         public static string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}";
         public static string Regex_EmailId = "^[A-Za-z0-9]+([.#_+-][0-9A-Za-z]+)*([@][A-Za-z0-9]+[.][A-Za-z]{2,3})([.][A-Za-z]{2})$";
         public static string Regex_MobileNumber = "^[1-9]{0,2}\\s[1-9]{1}[0-9]{9}$";
+        public static string Regex_Password = "^[0-9A-Za-z]{8,}$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
@@ -26,6 +27,10 @@ namespace UserRegistration
         public bool ValidateMobileNumber(string MobileNumber)
         {
             return Regex.IsMatch(MobileNumber, Regex_MobileNumber);
+        }
+        public bool ValidatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Password);
         }
     }
 }
