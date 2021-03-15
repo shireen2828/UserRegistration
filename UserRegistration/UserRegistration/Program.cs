062a59f941646fa6f19df7588637ef5e7fbef677
unknown <shireenkk28@gmail.com>
2021-03-15 23:29:21 +0530

    [shireen]Added FirstName

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validators validator = new Validators();
+            Console.WriteLine(validator.ValidateFirstName("Shireen"));
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegexValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class Validators
+    {
+        public static string Regex_FirstName = "^[A-Z]{1}[a-zA-Z]{2,}";
+        public bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, Regex_FirstName);
+        }
+    }
+}
